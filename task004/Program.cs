5ed8991c0c99f041def73bec388ebf4fe60c22e4
gabit <gabitteleubaev@gmail.com>
2023-01-06 10:18:51 +0500

    task004

---
+++ task004/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int [] Array = new int (num);
+int Array [0] = 0;
+int Array [1] = 1;
+int i = 2;
+
+while (i <num)
+{
+    Array[i] = Array [i] + i;
+    i++;
+
+}
+
+Console.WriteLine();
