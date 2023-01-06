3760280bb7fd1b627a9a221f527b6d23ae3c5eef
gabit <gabitteleubaev@gmail.com>
2023-01-06 10:02:40 +0500

    task002

---
+++ task003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+string Answer() //string Answer(int n); Таким оброзом программа не работает 
+{
+    int res = 0;
+    string answ = ""; 
+    while (num > 0)
+    {
+        res = num % 2;
+        num = num / 2;
+        answ = res + answ;
+    }
+    return answ;
+}
+ Console.Write(Answer()); //Console.Write(Answer(num)); Не работает
